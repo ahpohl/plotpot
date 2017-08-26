d792654130169db2b44758261827cc0c30fff5d8
Alexander Pohl <alex@ahpohl.com>
2017-08-26 22:37:01 +0200

    create journal objects

--- plotpot/data.py
+++ plotpot/data.py
@@ -7,6 +7,7 @@ import subprocess
 from distutils.spawn import find_executable
 import zipfile, tempfile, shutil
 from plotpot.dbmanager import DbManager
+from plotpot.journal import Journal
 
 
 # ignore division by zero errors
@@ -22,7 +23,8 @@ class Data(DbManager):
         super().__init__(self.dataFileName)
         self.callConvpot()
         self.reduceData()
-        self.fullStats = self.calcStatistics()
+        self.metaInfo = self.getMetaInfo()
+        #self.fullStats = self.calcStatistics()
         
     
     def reduceData(self):
@@ -42,7 +44,7 @@ class Data(DbManager):
     
     
     def getStatistics(self):
-        return self.stats
+        return self.fullStats
     
     
     def getPlots(self):
@@ -376,34 +378,6 @@ class Data(DbManager):
                 sys.exit(errormsg_not_recognised)
             
         return sorted(set(plots)) # remove duplicates and sort
-        
-    def getStatistics(self):
-        return self.fullStats
-    
-    
-    def dumpArray(self, fh, array, formatstring="%f", delimiter=',', header=''):
-        if formatstring == '%f':
-            formatspec = []
-            f = 0
-            while True:
-                if f == array.shape[1]:
-                    break
-                else:
-                    formatspec.append(formatstring)
-                f += 1
-        else:
-            formatspec = formatstring.split(' ')
-        if header != '':
-            fh.write(header)
-        for i in range(array.shape[0]):
-            for j in range(array.shape[1]):
-                if array[i,j] == None:
-                    array[i,j] = 0
-                if j == array.shape[1]-1:
-                    fh.write(formatspec[j] % (array[i,j]))
-                else:
-                    fh.write(formatspec[j] % (array[i,j])+delimiter)
-            fh.write("\n")
     
     
     def calcStatistics(self):
@@ -524,146 +498,61 @@ class Data(DbManager):
         
         return export
     
-    
-    def writeDataTable(self):
+
+    def getMetaInfo(self):
+        """Search journal for battery filename and date. If battery already exists
+           fetch meta info from journal. Ask questions to update
+           meta info according to selected plots.
+           If battery does not exist create new journal entry"""
+           
+        # create journal object
+        jourObj = Journal(self.args)
+           
+        # read file name and start datetime
+        fileNameDate = self.getNameAndDate()
+        fileCount = self.getFileCount()
         
-        with open(self.args.filename.split('.')[0]+'_data.csv', 'w') as fh:
-            header = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (
-                "data_point", "cycle_index", "step_index", 
-                "test_time", "step_time", "datetime", 
-                "current", "voltage", "capacity", 
-                "energy", "dQ/dV", "aux_channel")
-            fh.write(header)
-            header = ",,,%s,%s,%s,%s,%s,%s,%s,%s,\n" % (
-                "s", "s", "s", "A", "V", "As", "Ws", "As/V")
-            fh.write(header)        
-            self.__dump_array(fh, self.data, '%d %d %d %f %f %f %e %f %f %f %f %f')
-            fh.close()
-    
-    
-    def writeStatisticsTable(self):
+        if fileCount > 1:
+            fileNameList = list(fileNameDate)
+            fileNameList[0] = self.args.filename
+            fileNameDate = tuple(fileNameList)
         
-        # calculate mass loading
-        if self.metaInfo['area'] == 0:
-            loading = 0
+        # search plotpot-journal.dat if battery exists
+        searchResult = jourObj.searchJournal(fileNameDate)
+        
+        # if entry not found, fetch data from data file (global or file table)
+        if searchResult is None:
+            journalEntry = self.getFileDetails()
+            
+            # treat merged file different than single file
+            if fileCount > 1:
+                journalList = list(journalEntry)
+                journalList[1] = self.args.filename
+                journalList[6] = "merged"
+                journalEntry = tuple(journalList)
+              
         else:
-            # m / A = [ mg / cm² ]
-            loading = self.metaInfo['mass'] / self.metaInfo['area']
+            # get mass and capapcity from journal
+            journalEntry = searchResult
+            jourObj.setMetaInfo(journalEntry[6], journalEntry[7], journalEntry[8], journalEntry[9])
         
-        with open(self.args.filename.split('.')[0]+'_statistics.csv', 'w') as fh:
+        # get selected plots
+        plots = self.getPlots()
         
-            # export metaInfo
-            header = """Mass,Capacity,Area,Volume,Loading
-mg,mAh/g,cm²,µL,mg/cm²\n"""
-            fh.write(header)
+        # ask questions
+        if any([x in [1,4,5,11] for x in plots]):
+            jourObj.setMass()
+        if 10 in plots:
+            jourObj.setCapacity()
+        if any([x in [12] for x in plots]):
+            jourObj.setArea()
+        if 6 in plots:
+            jourObj.setVolume()
             
-            dat = "%.2f,%.2f,%.2f,%.2f,%.2f\n" % (
-                    self.metaInfo['mass'], 
-                    self.metaInfo['cap'], 
-                    self.metaInfo['area'], 
-                    self.metaInfo['volume'], 
-                    loading)
-            fh.write(dat)
-            
-            # write statistics
-            header = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (
-                    "cycle_index", "start", "end",
-                    "time(c)", "time(d)", 
-                    "capacity(c)", "capacity(d)", 
-                    "energy(c)", "energy(d)",
-                    "energy(c)", "energy(d)",
-                    "Vav(c)", "Vav(d)",
-                    "current(c)", "current(d)",
-                    "efficiency", "hysteresis",
-                    "density(c)", "density(d)",
-                    "density(c)", "density(d)",
-                    "c-rate(c)", "c-rate(d)")
-            fh.write(header)
-            
-            units = ",,,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (
-                    "s", "s", "mAh/g", "mAh/g", "Wh/kg",
-                    "Wh/kg", "Wh/L", "Wh/L", "V", "V", "A", "A", "%", "V",
-                    "mA/g", "mA/g", "mA/cm²", "mA/cm²", "h", "h")
-            fh.write(units)
-            
-            self.__dump_array(fh, self.expStats, "%d %d %d %f %f %f %f %f %f %f %f %f %f %e %e %f %f %f %f %f %f %f %f")
-            fh.close()
-    
-      
-    def writeVoltageProfile(self):
-        
-        # convert from As to mAh/g
-        if self.metaInfo['mass'] == 0:
-            self.data[:,8] = 0
+        # create new record in journal file if previous record was not found, otherwise update meta info
+        if searchResult == None:
+            jourObj.insertRow("Journal_Table", journalEntry)
         else:
-            self.data[:,8] = self.data[:,8] / (3.6e-3 * self.metaInfo['mass'])
+            jourObj.updateMetaInfo(fileNameDate)
             
-        cwd = os.getcwd() # save current directory
-        os.chdir(tempfile.gettempdir()) # change to tmp dir
-        filestem = self.args.filename.split('.')[0] # create filename
-        if not os.path.exists(filestem):
-            os.makedirs(filestem) # create directory
-        path = os.path.abspath(filestem) # change to directory
-        os.chdir(path)
-        
-        # loop over cycles
-        for c in self.cycles:
-            d = self.data[self.data[:,1]==c] # select cycle
-            ch = d[d[:,2]==1] # select charge half cycle
-            dc = d[d[:,2]==-1] # select discharge hafl cycle
-            ch = ch[:-1] # discard last data point, zero capacity
-            dc = dc[:-1] # discard last data point, zero capacity
-                
-            # charg
-            if ch.size:
-                header = """# cycle %d
-# charge
-# mass %.2f mg
-# %-12s %-12s %-12s
-# %-12s %-12s %-12s\n""" % (c, self.metaInfo['mass'], "capacity", "voltage", "dQ/dV", "mAh/g", "V", "As/V")
-                cyclename = filestem+'_%03d_charge.txt' % c
-                fh = open(cyclename, 'w')
-                fh.write(header)
-                for i in range(ch.shape[0]):
-                    line = "%12.6f %12.6f %12.6f\n" % (ch[i,8], ch[i,7], ch[i,10])
-                    fh.write(line)
-                fh.close()
-                
-            # discharge
-            if dc.size:
-                header = """# cycle %d
-# discharge
-# mass %.2f mg
-# %-12s %-12s %-12s
-# %-12s %-12s %-12s\n""" % (c, self.metaInfo['mass'], "capacity", "voltage", "dQ/dV", "mAh/g", "V", "As/V")
-                cyclename = filestem+'_%03d_discharge.txt' % c
-                fh = open(cyclename, 'w')
-                fh.write(header)
-                for i in range(dc.shape[0]):
-                    line = "%12.6f %12.6f %12.6f\n" % (dc[i,8], dc[i,7], dc[i,10])
-                    fh.write(line)
-                fh.close()
-                
-        # create zip archive
-        os.chdir(tempfile.gettempdir())
-        zfile = filestem + '.zip'
-        zipf = zipfile.ZipFile(zfile, 'w', zipfile.ZIP_DEFLATED)
-        base = os.path.basename(path)
-        for root, dirs, files in os.walk(path, topdown=False):
-            for name in files:
-                zipf.write(os.path.join(base, name))
-        zipf.close()
-        shutil.rmtree(path) # remove tmp directory
-        shutil.copy(zfile, cwd)
-        os.remove(zfile)
-        os.chdir(cwd)
-        
-        
-    def export(self, metaInfo):
-        """process --export option"""
-        self.metaInfo = metaInfo
-        self.writeStatisticsTable()
-        self.writeDataTable()
-        self.writeVoltageProfile()
-        self.writeMergeFileSummary()
-        
+        return jourObj.getMetaInfo()
--- plotpot/plotpot.py
+++ plotpot/plotpot.py
@@ -11,11 +11,10 @@ np.seterr(divide='ignore')
 
 # plotpot class
     
-class Plotpot(Journal):
+class Plotpot(object):
     
     def __init__(self, args):
         self.args = args
-        super().__init__(args)
         self.runSubcommands()        
 
     
@@ -32,12 +31,15 @@ class Plotpot(Journal):
     def subcommandJournal(self):
         """run journal subcommand"""
         
+        # create journal object
+        jourObj = Journal(self.args)
+        
         # print plotpot journal file
-        self.printJournal("Journal_Table")
+        jourObj.printJournal("Journal_Table")
         
         # delete journal entry
         if self.args.delete:
-            self.deleteRow("Journal_Table", self.args.delete)
+            jourObj.deleteRow("Journal_Table", self.args.delete)
         
         sys.exit() 
 
@@ -48,52 +50,7 @@ class Plotpot(Journal):
         # create plot object
         plotObj = Plot(self.args)
         
-        # read file name and start datetime
-        fileNameDate = plotObj.getNameAndDate()
-        fileCount = plotObj.getFileCount()
-        
-        if fileCount > 1:
-            fileNameList = list(fileNameDate)
-            fileNameList[0] = self.args.filename
-            fileNameDate = tuple(fileNameList)
-        
-        # search plotpot-journal.dat if battery exists
-        searchResult = self.searchJournal(fileNameDate)
-        
-        # if entry not found, fetch data from data file (global or file table)
-        if searchResult is None:
-            journalEntry = self.getFileDetails()
-            
-            # treat merged file different than single file
-            if fileCount > 1:
-                journalList = list(journalEntry)
-                journalList[1] = self.args.filename
-                journalList[6] = "merged"
-                journalEntry = tuple(journalList)
-              
-        else:
-            # get mass and capapcity from journal
-            journalEntry = searchResult
-            self.setMetaInfo(journalEntry[6], journalEntry[7], journalEntry[8], journalEntry[9])
-        
-        # get selected plots
-        plots = plotObj.getPlots()
-        
-        # ask questions
-        if any([x in [1,4,5,11] for x in plots]):
-            self.setMass()
-        if 10 in plots:
-            self.setCapacity()
-        if any([x in [12] for x in plots]):
-            self.setArea()
-        if 6 in plots:
-            self.setVolume()
-            
-        # create new record in journal file if previous record was not found, otherwise update meta info
-        if searchResult == None:
-            self.insertRow("Journal_Table", journalEntry)
-        else:
-            self.updateMetaInfo(fileNameDate)
+        sys.exit()
         
         # export  
         if self.args.export:
