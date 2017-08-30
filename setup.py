d7b918b59ee40a80b84d83b4fb887befcad8554e
Alexander Pohl <alex@ahpohl.com>
2017-08-30 22:24:52 +0200

    setup.py wheel include sample data

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['plotpot'],
+    packages=['plotpot', 'plotpot-sample'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -95,15 +95,15 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['arbintest.res'],
-    #},
+    package_data={
+        'plotpot-sample': ['*.res', '*.txt', '*.DTA'],
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
+    #data_files=[('plotpot', ['sample/*.res'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
